0b0a085418e5cce75500f6750de7de293d0806a1
HiccupAashih <shahaashish22@gmail.com>
2022-08-24 19:35:22 +1000

    Updates

---
+++ precache-manifest.647b9f42709c85c2a1e73f43e8b6bffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d8487085a8c963abd2f",
+    "url": "/music-app/static/css/main.cd9aea94.chunk.css"
+  },
+  {
+    "revision": "2d8487085a8c963abd2f",
+    "url": "/music-app/static/js/main.2d848708.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/music-app/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "65f3b48c67ddd875de13",
+    "url": "/music-app/static/js/2.65f3b48c.chunk.js"
+  },
+  {
+    "revision": "b4eca983afb9a1218963",
+    "url": "/music-app/static/js/runtime~main.b4eca983.js"
+  },
+  {
+    "revision": "45f6d567242d0c07cafdee08d33a5b5c",
+    "url": "/music-app/index.html"
+  }
+];
